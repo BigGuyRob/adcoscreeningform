17e0362998e4cf7849b805d7851ab6c44872d12d
BrainiacR <48625392+BrainiacR@users.noreply.github.com>
2020-06-22 13:31:45 -0400

    added app.js http create server

--- app.js
+++ app.js
@@ -1,13 +1,35 @@
-var MongoClient = require('mongodb').MongoClient;
-var url = "mongodb+srv://Rob:Cinderblock3@cluster0-jk0p0.mongodb.net/ADCOc19?retryWrites=true&w=majority";
+var http = require('http'); // 1 - Import Node.js core module
+const port = process.env.PORT || 5000;
 
-MongoClient.connect(url, function(err, db) {
-  if (err) throw err;
-  var dbo = db.db("ADCOc19");
-  var myobj = { name: "Company Inc", address: "Highway 37" };
-  dbo.collection("empInfo").insertOne(myobj, function(err, res) {
-    if (err) throw err;
-    console.log("1 document inserted");
-    db.close();
-  });
+const uri = "mongodb+srv://Rob:Cinderblock3@cluster0-jk0p0.mongodb.net/ADCOc19?retryWrites=true&w=majority";
+const client = new MongoClient(uri);
+
+
+
+var server = http.createServer(function (req, res) {   // 2 - creating server
+
+    if (req.method === 'POST') {
+    let body = '';
+    req.on('data', chunk => {
+        body += chunk.toString();
+    });
+    req.on('end', () => {
+        try {
+        // Connect to the MongoDB cluster
+        await client.connect();
+
+        // Make the appropriate DB calls
+
+    } finally {
+        // Close the connection to the MongoDB cluster
+        await client.close();
+    }
+
+	
+        res.end('ok');
+    });
+}
+
 });
+
+server.listen(port); //3 - listen for any incoming requests
